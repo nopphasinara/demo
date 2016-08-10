21912cb91fc8d8144a2131f938875d6dfb77c193
butschster <butschster@gmail.com>
2016-08-10 17:08:42 +0300

    Add test module

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -1,4 +1,3 @@
 <?php
 
-// PackageManager::load('admin-default')
-//    ->css('extend', resources_url('css/extend.css'));
+Meta::addJs('modules', asset('js/modules.js'), 'admin-default', true);
---
+++ public/js/modules.js
@@ -0,0 +1,61 @@
+/*
+ |--------------------------------------------------------------------------
+ | Modules:
+ |
+ | Bootstrap
+ | http://getbootstrap.com/javascript/
+ |
+ | DataTables
+ | https://datatables.net/manual/index
+ |
+ | Datetimepicker
+ | http://eonasdan.github.io/bootstrap-datetimepicker/
+ |
+ | DropzoneJS
+ | http://www.dropzonejs.com/
+ |
+ | i18next
+ | http://i18next.com/
+ |
+ | jQuery
+ | https://api.jquery.com/
+ |
+ | Magnific Popup
+ | http://dimsemenov.com/plugins/magnific-popup/documentation.html
+ |
+ | momentjs
+ | http://momentjs.com/docs/
+ |
+ | Nestable
+ | http://dbushell.github.io/Nestable/
+ |
+ | noty
+ | http://ned.im/noty/
+ |
+ | Select2
+ | https://select2.github.io/
+ |
+ | sweetalert2
+ | https://limonte.github.io/sweetalert2/
+ |
+ | VueJs
+ | https://vuejs.org/guide/
+ | https://github.com/vuejs/vue-resource/tree/master/docs
+ |
+ | x-editable
+ | https://vitalets.github.io/x-editable/
+ |
+ |--------------------------------------------------------------------------
+ */
+
+Admin.Modules
+    .add('new.element', function() {
+        // Place element code here
+    })
+
+    // You can replace existing module.
+    // For example switch to Chosen https://harvesthq.github.io/chosen/
+    //
+    // .add('form.elements.select', function() {
+    //     $('.input-select').chosen()
+    // });
